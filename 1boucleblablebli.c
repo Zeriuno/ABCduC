348a8b6173e9a78ca4e5047460d7360188ad9bea
Zeriuno <d.pitrolo@gmx.com>
2016-07-03 01:14:01 +0200

    blablebli bloblu

---
+++ 1boucleblablebli.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main ()
+{
+  for(int i=0 ; i<3 ; i++)
+  {
+    printf("bla")  ;
+    printf("ble")  ;
+    printf("bli ") ;
+  }
+  printf("\n")     ;
+  for(int i=0 ; i<6 ; i++)
+  {
+    printf("blo")  ;
+    printf("blu ") ;
+  }
+  return 0         ;
+}
