c0ac52cb7ad15aa897b9c0fbc4c501a015b59d67
Zeriuno <d.pitrolo@gmx.com>
2016-07-03 01:24:13 +0200

    Retour à la ligne oublié

---
+++ 1Onpeuttout.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  for(int i=0 ; i<3; i++)
+  {
+    printf("Le C, c'est la vie !\n") ;
+  }
+  printf("\n")                       ;
+  for(int i=0 ; i<5 ; i++)
+  {
+    printf("On peut tout faire avec !\n") ;
+  }
+}
+
