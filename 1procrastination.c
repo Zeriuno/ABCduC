2640c05c0810ca1c3f6295932a077ac5e841cf71
Zeriuno <d.pitrolo@gmx.com>
2016-07-03 02:27:37 +0200

    Calculs dans printf

---
+++ 1procrastination.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Cher procrastineur,\n") ;
+  printf("Il vous reste %d jours, soit %d minutes donc %d secondes avant de pouvoir procrastiner !", 25-23, 60*24*(25-23), 60*60*24*(25-23)) ;
+}
