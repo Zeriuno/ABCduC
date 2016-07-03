366d486b5850d5edf56a8c81807dd1790f3c9ec4
Zeriuno <d.pitrolo@gmx.com>
2016-07-03 02:02:23 +0200

    Censure du code

---
+++ 1censure.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Bienvenue, humains ! ");
+  printf("Je suis jarana, votre robot instructeur !\n");
+  printf("Aujourd'hui nous allons apprendre à aimer les robots :)\n");
+
+  printf("Répétez après moi : \n");
+  for (int i = 0; i < 3; i++)
+    printf("Kawai desne! \n");
+
+  printf("Toujours pas gentil ? ");
+  printf("Dans ce cas passons à la manière forte ...\n");
+  /*for (int i = 0; i<200; i++)
+  {
+    printf("Nous sommes venus en paix et avec bienveillance !\n");
+    printf("Un robot ne peut blesser un être humain ou, par son ");
+    printf("inaction, permettre qu'un être humain soit blessé.");
+  }*/
+  printf("C'était la fin de la leçon d'aujourd'hui !");
+  printf("Merci pour votre coopération, ");
+  //printf("au revoir !");
+  return 0;
+}
