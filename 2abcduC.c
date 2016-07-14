64531ed51d86bbefa74bcabee2d33bac9e23991b
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-14 13:06:42 +0200

    scanf et char

---
+++ 2abcduC.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+  char lettre1 = 'a', lettre2 = 'b', lettre3 = 'c';
+  printf("%c%c%c du langage C\n", lettre1, lettre2, lettre3);
+  return 0;
+}
---
+++ 2scanfespace.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  char un, deux;
+  scanf("%c %c", &un, &deux);
+  printf("%c%c", un, deux);
+  return 0;
+}
