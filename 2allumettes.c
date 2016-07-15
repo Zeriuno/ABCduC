48935cf7a6e2c20c995d1f4a0b81413989792e92
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-15 16:45:04 +0200

    Ranger les allumettes

---
+++ 2allumettes.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+  int allumettes, boites               ;
+
+  scanf("%d %d", &allumettes, &boites) ;
+  printf("%d\n", allumettes/boites)      ;
+  printf("%d\n", allumettes%boites)      ;
+  return 0                             ;
+}
