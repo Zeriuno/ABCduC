759af01a184dd482a506e84b5d6a98f3325ef994
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-15 17:40:50 +0200

    Arrondi et cast avant de la multiplication

--- 2ciment.c
+++ 2ciment.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-  double ciment                  ;
-  int kilosac = 60, prixsac = 45 ;
-  scanf("%lf", &ciment)          ;
-  int cout = (int) (ciment / kilosac +1) * prixsac;
-  printf("%d\n", cout);
+  double ciment                     ;
+  int kilosac = 60, prixsac = 45, sacs ;
+  scanf("%lf", &ciment)             ;
+  sacs = (int) ciment / kilosac +1  ;
+  printf("%d\n", sacs*prixsac)      ;
   return 0;
 }
