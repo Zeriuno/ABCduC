5756c5f1aeddfe5e1c82efa67e5d9874e306d6f9
Daniele Pitrolo <d.pitrolo@gmx.com>
2016-07-19 11:40:21 +0200

    Carré d'étoiles - boucle  pour le nombre d'éléments sur chaque ligne, dans une boucle  pour le nombre de lignes

---
+++ 3etoiles.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+  int taille, i, j ;
+  scanf("%d", &taille) ;
+  taille++ ;
+  for(i = 0 ; i < taille ;  i++)
+  {
+    for(j = 0 ; j < taille ; j++)
+    {
+      printf("*") ;
+    }
+    printf("\n")  ;
+  }
+  return 0 ;
+}
